a5ecb63c9cc9793b7b7667cb44df6f13c4e02ee9
kevinjobs <kevinjobs@qq.com>
2021-04-05 00:35:20 +0800

    fix: 修复 log 列表过长的问题

--- src/pages/admin/logs/index.tsx
+++ src/pages/admin/logs/index.tsx
@@ -1,23 +1,33 @@
 import React from 'react';
+import dayjs from 'dayjs';
 import { LogApi } from '@/apis';
 
 export interface AdminLogPageProps {};
 
 const AdminLogPage: React.FC<AdminLogPageProps> = props => {
+  const today = dayjs().format('YYYY-MM-DD');
+
   const [logs, setLogs] = React.useState<string[]>();
-  const [log, setLog] = React.useState();
+  const [log, setLog] = React.useState<string | undefined>();
 
   React.useEffect(() => {
     LogApi.getList().then(res => {
       if (res.status === 200) {
         if (res.data.code === 1) {
           setLogs(res.data.data);
-          setLog(res.data.data[0]);
         }
       }
     }).catch(err => {
       if (err.response.status === 401) {
         window.alert(err.response.data.msg);
+      }
+    });
+
+    LogApi.getByFileName(today + '.log').then(res => {
+      if (res.status === 200) {
+        if (res.data.code === 1) {
+          setLog(res.data.data);
+        }
       }
     })
   }, [])
@@ -53,15 +63,17 @@ const AdminLogPage: React.FC<AdminLogPageProps> = props => {
         <h3>日志查看</h3>
       </div>
       <div className="container">
-        <table>
-          <tbody>
-            <tr>
-              <th>序号</th>
-              <th>文件名</th>
-            </tr>
-            { logs?.map(renderItem) }
-          </tbody>
-        </table>
+        <div className="log-list">
+          <table>
+            <tbody>
+              <tr>
+                <th>序号</th>
+                <th>文件名</th>
+              </tr>
+              { logs?.map(renderItem) }
+            </tbody>
+          </table>
+        </div>
         <div className="log-viewer">
           <div>{ log ? log : <div className="mint-loader"></div> }</div>
         </div>
