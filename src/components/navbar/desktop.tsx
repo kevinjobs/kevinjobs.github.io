ba0fb5e76360654007d4aa591bfda1263fc9c869
kevinjobs <kevinjobs@qq.com>
2021-04-07 20:29:53 +0800

    perf(组件): 调整桌面导航栏样式

--- src/components/navbar/desktop.tsx
+++ src/components/navbar/desktop.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import multiavatar from '@multiavatar/multiavatar';
-import { Button, NavbarProps, Switch } from '@/components';
+import { Button, NavbarProps, Switch, Icon, Transition } from '@/components';
 import { UserInterface } from '@/types';
 import classNames from 'classnames';
 import { useTheme } from '@/hooks';
 
 const DesktopNavbar: React.FC<NavbarProps> = (props) => {
   const [userInfo, setUserInfo] = React.useState<UserInterface>();
+  const [menuItemShow, setMenuItemShow] = React.useState(false);
 
   const { fresh, onLogin, onLogout, onSwitchTheme, menus } = props;
 
@@ -16,31 +17,58 @@ const DesktopNavbar: React.FC<NavbarProps> = (props) => {
     setUserInfo(JSON.parse(user!));
   }, [fresh]);
 
-  const renderMenu = (menu: string, index: number) => {
-    return (
-      <li key={index}>
-        <NavLink to={'/' + menu}>{menu}</NavLink>
-      </li>
-    )
-  }
-
   const renderLoginButton = () => (
     <div className="DesktopNavbar--Container__Others--Login">
       <Button type="primary" onClick={onLogin}>登录</Button>
     </div>
   )
 
-  const renderUserInfo = () => {
-    const Roles: {
-      [key: string]: any
-    } = {
-      0: '超级用户',
-      1: '管理员',
-      2: '还没想好',
-      3: 'VIP',
-      4: '普通用户'
-    }
+  const renderMenus = () => {
+    return (
+      <div className="DesktopNavbar--Container__Menus">
+        <div className="menu-item">
+          <span className="title">
+            <Link to="/">Mint Forge</Link>
+          </span>
+        </div>
+        <div className="menu-item">
+          <span className="title">
+            <Link to="/schedule">待办事项</Link>
+          </span>
+        </div>
+        <div className="menu-item"
+          onMouseEnter={e => setMenuItemShow(true)}
+          onMouseLeave={e => setMenuItemShow(false)}
+        >
+          <span className="title">
+            探索更多
+            <span className="down-arrow">
+              <Icon icon="chevron-down" style={{color: '#f1f1f1', fontSize: 16}} />
+            </span>
+          </span>
+          <Transition className="more" timeout={100} in={menuItemShow} animation="FadeInOut">
+            <div>
+              <div className="more-item">
+                <Link to="/wiki">百科</Link>
+              </div>
+              <div className="more-item">
+                <Link to="/home">文章</Link>
+              </div>
+              <div className="more-item">
+                <Link to="/gallery">相册</Link>
+              </div>
+              <div className="divider"></div>
+              <div className="more-item">
+                <Link to="/admin">管理</Link>
+              </div>
+            </div>
+          </Transition>
+        </div>
+      </div>
+    )
+  }
 
+  const renderUserInfo = () => {
     const user = userInfo;
 
     if (user) {
@@ -50,7 +78,7 @@ const DesktopNavbar: React.FC<NavbarProps> = (props) => {
             dangerouslySetInnerHTML={{__html: multiavatar(user.username)}}></div>
           <div className="infos">
             <div className="item">{user.username}</div>
-            <div className="item">{Roles[user.role]}</div>
+            <div className="item">{user.role}</div>
             <div className="item">
               <Link to={`/profile/${user.username}`}>个人中心</Link>
             </div>
@@ -70,7 +98,6 @@ const DesktopNavbar: React.FC<NavbarProps> = (props) => {
   const { theme } = useTheme();
   const classname = classNames(
     'DesktopNavbar',
-    'shadow-card',
     {
       [`theme-${theme}`]: theme
     }
@@ -79,9 +106,7 @@ const DesktopNavbar: React.FC<NavbarProps> = (props) => {
   return(
     <div className={classname}>
       <div className="DesktopNavbar--Container">
-        <ul className="DesktopNavbar--Container__Menus">
-          { menus && menus.map(renderMenu) }
-        </ul>
+        { renderMenus() }
         <div className="DesktopNavbar--Container__Others">
           <div className="DesktopNavbar--Container__Others--SearchBar"></div>
           { userInfo ? renderUserInfo() : renderLoginButton() }
