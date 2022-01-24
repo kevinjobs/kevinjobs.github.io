ddce4e0baa5a860a0ed2cce49a35d22eee834c51
Kevin Jobs <kevinjobs@qq.com>
2022-01-24 18:26:55 +0800

    fix 将 nojekyll 文件复制到正确的地方

--- deploy.js
+++ deploy.js
@@ -1,7 +1,7 @@
 /*
  * @Author       : Kevin Jobs
  * @Date         : 2022-01-24 10:43:56
- * @LastEditTime : 2022-01-24 18:03:15
+ * @LastEditTime : 2022-01-24 18:24:43
  * @lastEditors  : Kevin Jobs
  * @FilePath     : \kevinjobs.github.io\deploy.js
  * @Description  : 
@@ -10,16 +10,34 @@ const ghpages = require('gh-pages');
 const fs = require('fs');
 
 // 避免 github pages 忽略该文件夹
-fs.copyFileSync('./.nojekyll', './out/_next/.nojekyll');
+fs.copyFileSync('./.nojekyll', './out/.nojekyll');
+
+const now = getNow();
 
 ghpages.publish('out', {
   branch: 'gh-pages',
   repo: 'git@github.com:kevinjobs/kevinjobs.github.io.git',
-  message: `update ${new Date().valueOf()}`
+  message: `update ${now}`,
+  dotfiles: true,
 }, function(err) {
   if (err) {
     console.log(err);
   } else {
-    console.log('deploy success.');
+    console.log('deploy success: ' + now);
   }
 });
+
+function getNow() {
+  const d = new Date();
+  let month = d.getMonth() + 1;
+  let day = d.getDate();
+  const year = d.getFullYear();
+  const hour = d.getHours();
+  const min = d.getMinutes();
+  const s = d.getSeconds();
+  
+  if (month.length < 2) month = '0' + month;
+  if (day.length < 2) day = '0' + day;
+
+  return [year, month, day].join('-') + ' ' + [hour, min, s].join(':');
+}
---
+++ public/README.md
@@ -0,0 +1,1 @@
+This is my blog powered by NEXT.js
