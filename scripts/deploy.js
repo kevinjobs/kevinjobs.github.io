47db04db0db7229dae0942b01bbc302d27e81002
Kevin Jobs <me@kevinjobs.com>
2022-08-14 20:51:51 +0800

    refactor: use monorepo

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -9,12 +9,17 @@
 const ghpages = require('gh-pages');
 const fs = require('fs');
 const deployConfig = require('../deploy.config');
+const path = require("path");
+
+const distDir = path.join(__dirname, "../dist");
 
 // 默认情况下 github pages 会忽略点号开头的文件夹和文件夹
 // 在根目录创建一个 .nojekyll 可以避免这个行为
-fs.copyFileSync('./.nojekyll', './out/.nojekyll');
+const nojekyll = path.join(__dirname, "../.nojekyll");
+const distNojekyll = path.join(distDir, ".nojekyll");
+fs.copyFileSync(nojekyll, distNojekyll);
 
-ghpages.publish('out', deployConfig, function(err) {
+ghpages.publish(distDir, deployConfig, function(err) {
   if (err) {
     console.error(err);
   } else {
